a598d2559adf8deea32a801c9da7f90008b966b6
Allisonu0630 <allison.unruh@ucalgary.ca>
2025-09-14 17:54:20 -0600

    Update driver.py

--- examples/driver.py
+++ examples/driver.py
@@ -2,11 +2,11 @@ import numpy as np
 from my_python_package.operators import add, multiply
 
 def main():
-    # test for scalars
+   
     print(f'add(1, 3): {add(1, 3)}')
     print(f'multiply(2, 12.): {multiply(2, 12.)}')
     
-    # test for arrays
+    
     A = np.array([[1, 2, 3], [4, 5, 6]])
     B = 2. * np.ones(A.shape)
     print(f'A:\n{A}')
